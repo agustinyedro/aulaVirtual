888f4a9ae9e432ce76ab24825e6de846f3a558b6
Agustin Yedro <76633203+agustinyedro@users.noreply.github.com>
2024-02-07 00:16:51 -0300

    autentificacion con supabase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 
-import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), auth()],
+  integrations: [tailwind(),],
   i18n: {
     defaultLocale: 'es',
     locales: ['es'],
     routing: {
       prefixDefaultLocale: false
     }
-  }
+  },
+  output: "server",
+  adapter: vercel(),
 });
